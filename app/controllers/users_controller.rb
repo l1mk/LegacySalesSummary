98dfc1097097cb2f290d1e97d2c1669fabb2424a
github username <github email address>
2020-03-13 13:38:56 -0500

    edit for user working perfectly

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,10 +19,26 @@ class UsersController < ApplicationController
     end
 
     def edit
+        @user = User.find(params[:id])
     end
 
     def update
+        @user = User.find(params[:id])
+        @user.update(user_params)
+        if @user.save 
+            redirect_to @user 
+        else
+            render :edit
+        end
     end
+
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy 
+        flash[:notice] = "User deleted"
+        redirect_to users_path
+    end
+    
 
     private 
     def user_params 
