94ef44b8e3935513135fe3db3ac3e407f572b124
github username <github email address>
2020-03-19 17:15:24 -0500

    farm model and views ready and functioning

--- app/controllers/farms_controller.rb
+++ app/controllers/farms_controller.rb
@@ -1,2 +1,57 @@
 class FarmsController < ApplicationController
+        def index
+            @farms = Farm.all 
+            @week = Week.find_by(id: params[:id])
+        end
+    
+        def new
+            @farm = Farm.new
+            @week = Week.find_by(id: params[:id])
+            @clients = Client.all 
+        end
+    
+        def create
+            @clients = Client.all
+            @farm = Farm.new(farm_params)
+            @farm.client = Client.find_by(id: params[:client_id])
+            if @farm.save 
+                redirect_to user_week_farms_url
+            else
+                render :new 
+            end
+        end
+    
+        def show
+            @farm = Farm.find_by(id: params[:id])
+            @week = Week.find_by(id: @farm.week_id)
+            @client = Client.find_by(id: @farm.client_id) 
+        end
+    
+        def edit
+            @farm = Farm.find(params[:id])
+        end
+    
+        def update
+            @farm = Farm.find(params[:id])
+            @farm.update(farm_params)
+            if @farm.save 
+                redirect_to user_week_farms_url
+            else
+                render :edit
+            end
+        end
+    
+        def destroy
+            @farm = Farm.find(params[:id])
+            @farm.destroy 
+            flash[:notice] = "Farm deleted"
+            redirect_to user_week_farms_path
+        end
+    
+    
+        private 
+        def farm_params 
+            params.require(:farm).permit(:amount, :recap, :date_of_visit, :week_id, :client_id)
+        end
+
 end
