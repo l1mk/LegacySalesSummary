e88f8e4132692991516a5ca8bd258aaf780aa9c5
github username <github email address>
2020-04-17 21:27:08 -0500

    added helper for updated at and created at, added missing link to delete in client show

--- app/helpers/clients_helper.rb
+++ app/helpers/clients_helper.rb
@@ -1,2 +1,5 @@
 module ClientsHelper
+    def created_at(c)
+        c.created_at.strftime("%A, %b %e, at %l:%M %p")
+    end
 end
--- app/helpers/haunteds_helper.rb
+++ app/helpers/haunteds_helper.rb
@@ -1,2 +1,6 @@
 module HauntedsHelper
+    def last_updated_haunted(h)
+        h.updated_at.strftime("Last updated %A, %b %e, at %l:%M %p")
+      end
+
 end
